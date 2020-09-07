0d3b41532c425fbeed3007ca588229aa91cb78f8
verzada <harald.s.hanssen@gmail.com>
2020-09-07 11:15:39 +0200

    #1 Organized stuff a bit more, makes the installer a tad slower, but makes it easier to see what should be optional

--- ChocoInstallSoftware.ps1
+++ ChocoInstallSoftware.ps1
@@ -1,3 +1,10 @@
-choco upgrade gitextensions slack vscode soapui visualstudio2019community
+#default stuff 
+choco upgrade gitextensions slack vscode soapui 
 choco upgrade notepadplusplus -x86
+
+#web browsers
+choco upgrade googlechrome vivaldi firefox
+
+#.NET 
+choco upgrade visualstudio2019community  
 choco upgrade resharper-ultimate-all --params "'/PerMachine /NoCpp /NoTeamCityAddin"
