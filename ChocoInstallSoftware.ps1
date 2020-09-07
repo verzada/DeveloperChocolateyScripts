0e7e40c90c398127bbf1eca41f295bb73303b786
verzada <harald.s.hanssen@gmail.com>
2020-09-07 11:09:09 +0200

    #1 Added Resharper to choco installer

--- ChocoInstallSoftware.ps1
+++ ChocoInstallSoftware.ps1
@@ -1,2 +1,3 @@
 choco upgrade gitextensions slack vscode soapui visualstudio2019community
 choco upgrade notepadplusplus -x86
+choco upgrade resharper-ultimate-all --params "'/PerMachine /NoCpp /NoTeamCityAddin"
